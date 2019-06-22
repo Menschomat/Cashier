39c4a7d3c71bccad919b975cc24aca0283bdfb7f
Falco Weihrauch <falco@robaux.de>
2019-06-22 19:06:59 +0200

    Refactoring + First Test

---
+++ cashierBack/src/main/java/de/menschomat/wgo/CashierApplication.java
@@ -0,0 +1,14 @@
+package de.menschomat.wgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CashierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashierApplication.class, args);
+    }
+
+}
--- cashierBack/src/test/java/de/menschomat/wgo/CashierApplicationTests.java
+++ cashierBack/src/test/java/de/menschomat/wgo/CashierApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WgoApplicationTests {
+public class CashierApplicationTests {
 
     @Test
     public void contextLoads() {
